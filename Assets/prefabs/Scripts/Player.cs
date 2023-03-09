24326bca18bf17dd76655b4c71d9bfcd821ae46a
Aarnav Srivastava <aarnav.srivastava18@gmail.com>
2023-03-09 12:18:31 -0500

    AAAAAAAAAAAAAAAAAAAAAA

--- Assets/prefabs/Scripts/Player.cs
+++ Assets/prefabs/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,49 +9,108 @@ public class Player : MonoBehaviour
     public Camera camera;
     Rigidbody rigidbody;
 
-    private Vector3 start;
-    private Vector3 des;
-    private float fraction = 0;
+    public GameObject engine1;
+    public GameObject engine2;
+
+    public GameObject reticle;
+
+    float velocityxy = 100;
+    float velocityz = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = gameObject.GetComponent<Rigidbody>();
-
-        start = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        des = new Vector3(transform.position.x, 10f, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && transform.position.y < 20)
+        rigidbody.AddForce(Vector3.forward * velocityz, ForceMode.Impulse);
+        if (Input.GetKey(KeyCode.W) && transform.position.y < 15f)
         {
-            rigidbody.AddForce(Vector3.up * 10, ForceMode.Acceleration);
+            rigidbody.AddForce(Vector3.up * velocityxy, ForceMode.Acceleration);
         }
-        else if (transform.position.y >= 20)
+        else if (transform.position.y >= 15)
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-            transform.position = new Vector3(transform.position.x, 19.99999f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, 15f, transform.position.z);
         }
         else
         {
             rigidbody.velocity = rigidbody.velocity * 0.95f * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.S) && transform.position.y > -20)
+        if (Input.GetKey(KeyCode.S) && transform.position.y > -15f)
         {
-            rigidbody.AddForce(Vector3.down * 10, ForceMode.Acceleration);
+            rigidbody.AddForce(Vector3.down * velocityxy, ForceMode.Acceleration);
         }
-        else if (transform.position.y <= -20)
+        else if (transform.position.y <= -15f)
         {
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-            transform.position = new Vector3(transform.position.x, -19.99999f, transform.position.z);
+            transform.position = new Vector3(transform.position.x, -15f, transform.position.z);
         }
         else
         {
             rigidbody.velocity = rigidbody.velocity * 0.95f * Time.deltaTime;
         }
 
-        camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(camera.transform.position.x, transform.position.y + 6, transform.position.z - 30), Time.deltaTime * 0.5f);
+        if (Input.GetKey(KeyCode.A) && transform.position.x > -30)
+        {
+            rigidbody.AddForce(Vector3.left * velocityxy, ForceMode.Acceleration);
+        }
+        else if (transform.position.x <= -30)
+        {
+            rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, rigidbody.velocity.z);
+            transform.position = new Vector3(-30, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            rigidbody.velocity = rigidbody.velocity * 0.95f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D) && transform.position.x < 30)
+        {
+            rigidbody.AddForce(Vector3.right * velocityxy, ForceMode.Acceleration);
+        }
+        else if (transform.position.x >= 30)
+        {
+            rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, rigidbody.velocity.z);
+            transform.position = new Vector3(30, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            rigidbody.velocity = rigidbody.velocity * 0.95f * Time.deltaTime;
+        }
+
+        if (!Input.anyKey)
+        {
+            setMin();
+        }
+        else
+            setMax();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            velocityz = rigidbody.velocity * 0.95f * Time.deltaTime;
+        }
+
+        var rotationAngle = Quaternion.LookRotation(reticle.transform.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * 10);
+
+        camera.transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y, transform.position.z - 40);
+        reticle.transform.position = new Vector3(reticle.transform.position.x, reticle.transform.position.y, transform.position.z + 50);
+    }
+
+    void setMin()
+    {
+        engine1.GetComponent<ParticleSystem>().startSize = 5;
+        engine2.GetComponent<ParticleSystem>().startSize = 5;
+    }
+
+    void setMax()
+    {
+        engine1.GetComponent<ParticleSystem>().startSize = 7;
+        engine2.GetComponent<ParticleSystem>().startSize = 7;
     }
 }
